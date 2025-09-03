210eda903a83853acc8ad63af989ac9e5e3c546f
Zeeshan Ahmad <itexpert120@outlook.com>
2025-09-03 23:27:37 +0500

    feat: make lighting up nights responsive

--- src/components/sections/home/lighting-up-nights.tsx
+++ src/components/sections/home/lighting-up-nights.tsx
@@ -5,31 +5,31 @@ export function LightingUpNights() {
   return (
     <section
       id="lighting-up-nights"
-      className="flex flex-col items-center justify-center relative"
+      className="flex flex-col items-center justify-center relative px-4 sm:px-0"
     >
       <Image
         src="/images/home/lighting-up-nights/flakes.png"
         alt="Flakes"
-        className="absolute inset-0 left-1/2 top-0 -translate-x-1/2 pointer-events-none max-w-[1440px] mix-blend-overlay opacity-20"
+        className="absolute inset-0 left-1/2 top-0 -translate-x-1/2 pointer-events-none w-full max-w-[1440px] mix-blend-overlay opacity-20"
         width={1440}
         height={1440}
       />
-      <h2 className="relative text-[64px] leading-[74px] tracking-[3.2px] font-marlton text-center pt-[144px] pb-[82px] text-transparent">
+      <h2 className="relative text-[32px] sm:text-[48px] lg:text-[64px] leading-[38px] sm:leading-[56px] lg:leading-[74px] tracking-[1.6px] sm:tracking-[2.4px] lg:tracking-[3.2px] font-marlton text-center pt-16 sm:pt-24 lg:pt-[144px] pb-10 sm:pb-16 lg:pb-[82px] text-transparent">
         <span>LIGHTING UP NIGHTS</span> <br />
         <span>SPREADING HOLIDAY JOY</span>
         <img
           src="/images/home/lighting-up-nights/heading.png"
-          className="absolute top-1/2 -translate-y-9 pointer-events-none"
+          className="absolute top-1/2 -translate-y-4.5 sm:-translate-y-6.75 lg:-translate-y-9 pointer-events-none w-full max-w-[320px] sm:max-w-[480px] lg:max-w-none"
           alt="Heading"
         />
       </h2>
-      <div className="relative w-full h-[669.884px] mb-[102px]">
+      <div className="relative w-full h-[200px] sm:h-[400px] lg:h-[669.884px] mb-16 sm:mb-20 lg:mb-[102px]">
         <Image
           src="/images/home/lighting-up-nights/images.png"
           alt="Images"
           width={2100}
           height={669.884}
-          className="w-[2100px] h-full mx-auto object-cover"
+          className="w-full h-full mx-auto object-cover"
         />
       </div>
     </section>
